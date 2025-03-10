1008c9771d017bfbf685bbf51d9a25779cfb0440
goutam kumar <qwertyeng100@gmail.com>
2025-03-11 00:24:50 +0530

    Adding some utils files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@
 import dotenv from "dotenv";
 // import mongoose from "mongoose";
 import connectDB from "./db/index.js";
-
+import express from express;
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+     app.on('error',(error)=>{
+          console.log("Error",error);
+     })
+     
+     app.listen(process.env.PORT || 3000,()=>{
+          console.log(`server is running at port ${process.env.port || 3000}`);
+     })
+})
+.catch((error)=>{
+     console.log(`Mongo db connection failed:${error}`);
+     
+})
